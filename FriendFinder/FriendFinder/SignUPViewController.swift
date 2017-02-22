14bf8690ceb6e40eba4967a0957134fc0feed79b
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-02-22 17:51:41 +0530

    Signup

--- FriendFinder/FriendFinder/SignUPViewController.swift
+++ FriendFinder/FriendFinder/SignUPViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class SignUPViewController: UIViewController {
     
-    @IBOutlet var fet: UIButton!
         @IBOutlet var firstname: UITextField!
         @IBOutlet var lastname: UITextField!
         @IBOutlet var username: UITextField!
@@ -25,11 +24,12 @@ class SignUPViewController: UIViewController {
             self.title = "SIGN UP"
             self.navigationController?.navigationBar.isHidden = true
             btnsubmit.layer.cornerRadius = 8
-
+            
         }
         
         //MARK: - Button Action
         @IBAction func signUPAction(_ sender: UIButton) {
+            
             if self.firstname.text!.isEmpty || self.lastname.text!.isEmpty || self.username.text!.isEmpty || self.pwd.text!.isEmpty || self.pwd1.text!.isEmpty {
                 let alt = UIAlertController(title: "Enter Value", message: "All Fields are Mandatory", preferredStyle: UIAlertControllerStyle.alert)
                 alt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
@@ -45,103 +45,23 @@ class SignUPViewController: UIViewController {
                 self.navigationController?.pushViewController(vc, animated: true)
             }
             
-                let link = "http://127.0.0.1:8085/test"
-            let parameters = ["name":"Abhishek1","surname": "ABCD","username":"1234", "password":"Master1"] as Dictionary<String, String>
-                let request = NSMutableURLRequest(url: NSURL(string: link)! as URL)
+            let parameters = ["name": self.firstname.text!,"surname": self.lastname.text!,"username": self.username.text!,"password": self.pwd.text!]
                 
-                let session = URLSession.shared
-                request.httpMethod = "POST"
+                server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "reg", Request_parameter: parameters, Request_parameter_Images: nil, status: { (result) in
                 
-                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.addValue("application/json", forHTTPHeaderField: "Accept")
-                
-                request.httpBody = try! JSONSerialization.data(withJSONObject: parameters, options: [])
-                
-                let task = session.dataTask(with: request as URLRequest) { data, response, error in
-                    guard data != nil else {
-                        print("no data found: \(error)")
-                        return
-                    }
-                    
-                    do {
-                        if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
-                            print("Response: \(json)")
-                        } else {
-                            let jsonStr = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                            print("Error could not parse JSON: \(jsonStr)")
-                        }
-                    } catch let parseError {
-                        print(parseError)
-                        let jsonStr = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                        print("Error could not parse JSON: '\(jsonStr)'")
+            }, response_Dictionary: { (json) in
+                DispatchQueue.main.async {
+                    if(json.value(forKey: "resp") as! String == "Success"){
+                        self.navigationController?.pushViewController(LoginViewController(nibName: "LoginViewController", bundle: nil), animated: true)
                     }
                 }
                 
-                task.resume()
-            
+            }, response_Array: { (resultsArr) in
                 
+            }, isTokenEmbeded: false)
+            
             
         }
-    
-    @IBAction func fet(_ sender: Any) {
-            let link = "http://127.0.0.1:8085/fetch/"
-            let url:URL = URL(string: link)!
-            let session = URLSession.shared
-            let request = NSMutableURLRequest(url: url)
-            request.httpMethod = "GET"
-            request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-            let task = session.dataTask(with: request as URLRequest, completionHandler: {
-                (
-                data, response, error) in
-                guard let _:Data = data, let _:URLResponse = response  , error == nil else {
-                    return
-                }
-                self.extract_json(data!)
-            })
-            task.resume()
-    }
-    
-        func extract_json(_ data: Data)
-        {
-            print(data)
-            let json: Any?
-            do
-            {
-                json = try JSONSerialization.jsonObject(with: data, options: [])
-            }
-            catch
-            {
-                return
-            }
- 
-            guard let data_list = json as? NSArray else
-            {
-                return
-            }
-            if let countries_list = json as? NSArray
-            {
-                for i in 0 ..< data_list.count
-                {
-                    if let country_obj = countries_list[i] as? NSDictionary
-                    {
-                        if let country_name = country_obj["name"] as? String
-                        {
-                            if let country_code = country_obj["password"] as? String
-                            {
-                                
-                                TableData.append("Name: " + country_name + "Password: " + country_code)
-                                print(country_obj)
-                            }
-                        }
-                    }
-                }
-            }
-            
-            DispatchQueue.main.async(execute: {})
-            
-        }
-
-    
     
 }
 
