3bdccfc0054e0c2a136395f324455fa83d67ceca
smartsuhani <lanetteam.suhani@gmail.com>
2017-02-24 17:43:59 +0530

    mapView

--- FriendFinder/HomeViewController.swift
+++ FriendFinder/HomeViewController.swift
@@ -1,18 +1,18 @@
 import UIKit
 import MapKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
 
     @IBOutlet var lblName: UILabel!
     @IBOutlet var mapCurrentLocation: MKMapView!
     @IBOutlet var lblAddress: UILabel!
     @IBOutlet var lblUsername: UILabel!
     @IBOutlet var imgProfile: UIImageView!
-    @IBOutlet var barNavigate: UITabBar!
     
     let defaults = UserDefaults.standard
     var friendlist: NSMutableArray!
     var userdetail: NSDictionary!
+    let locManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +20,46 @@ class HomeViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
         
         mapCurrentLocation.layer.cornerRadius = mapCurrentLocation.frame.height / 2
-//        barNavigate.image?.alignmentRectInsets.bottom
         imgProfile.image = genQRCode()
         fetchData()
+        
+        locManager.delegate = self
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        locManager.requestWhenInUseAuthorization()
+        locManager.startUpdatingLocation()
+        print(locManager.location!)
+        let a = locManager.location!.coordinate.latitude
+        let b = locManager.location!.coordinate.longitude
+        
+        let location = CLLocationCoordinate2DMake(a,b)
+        let annotation = MKPointAnnotation()
+        mapCurrentLocation.delegate = self
+        mapCurrentLocation.removeAnnotation(annotation as MKAnnotation)
+        annotation.coordinate = location
+        annotation.title = "Current Location"
+        mapCurrentLocation.addAnnotation(annotation)
+        
+        mapCurrentLocation.isScrollEnabled = false
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(location.coordinate, span)
+            mapCurrentLocation.setRegion(region, animated: true)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            locManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
     }
     
@@ -38,13 +75,16 @@ class HomeViewController: UIViewController {
             DispatchQueue.main.async {
                 print(resultsArr)
                 self.userdetail = (resultsArr.object(at: 0) as AnyObject) as! NSDictionary
-//                self.lblUsername.text = "Username: " + (self.userdetail.value(forKey: "username") as? String)!
+                self.lblUsername.text = "Username: " + (self.userdetail.value(forKey: "username") as? String)!
                 self.lblName.text = (self.userdetail.value(forKey: "name") as? String)!
                 self.lblAddress.text = (self.userdetail.value(forKey: "locality") as? String)!
                 
             }
         }, isTokenEmbeded: false)
         
+    }
+    @IBAction func Friends(_ sender: Any) {
+        self.navigationController?.pushViewController(FriendViewController(), animated: true)
     }
     func genQRCode() -> UIImage? {
         let data = (defaults.value(forKey: "user")as! String).data(using: String.Encoding.ascii)
