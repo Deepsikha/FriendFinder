2a333657f06baad4f1455236ed801985475bf70f
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-02-25 18:16:18 +0530

    efsdfds

--- FriendFinder/FriendFinder/Location.swift
+++ FriendFinder/FriendFinder/Location.swift
@@ -1,53 +1,40 @@
-//
-//  Location.swift
-//  FriendFinder
-//
-//  Created by Developer88 on 2/24/17.
-//  Copyright © 2017 LaNet. All rights reserved.
-//
-
 import Foundation
 import CoreLocation
 
 class Location : NSObject, CLLocationManagerDelegate{
     var locationManager: CLLocationManager = CLLocationManager()
     var defaults = UserDefaults.standard
-    var lat : String!
-    var long : String!
-    var location : String!
-    var user : String!
-    var parameters : [String:String]!
     
     override init(){
-    super.init()
-        
+        super.init()
+        print("Location")
         locationManager.delegate = self
         locationManager.requestAlwaysAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.distanceFilter = 20
-        locationManager.startUpdatingLocation()
-        lat = String(describing: (locationManager.location?.coordinate.latitude)!)
-        long = String(describing: (locationManager.location?.coordinate.longitude)!)
-        location = lat.appending(",").appending(long)
-        print(location)
-        user = defaults.string(forKey: "user")
-        parameters = ["username" : user!,"location" : location]
-    server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "loc", Request_parameter: parameters, Request_parameter_Images: nil, status: { (request) in
-            
-        }, response_Dictionary: { (request) in
-            
-        }, response_Array: { (request) in
-            
-        }, isTokenEmbeded: false)
-
+        locationManager.distanceFilter = 150.0
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization() // or request always if you need it
+        case .restricted, .denied: break
+        // tell users that they need to enable access in settings
+        default:
+            break
+        }
+        
+        
     }
     func locationManager(_ manager: CLLocationManager,
                          didUpdateLocations locations: [CLLocation])
     {
-        print("12345678")
-        user = defaults.string(forKey: "user")
-        parameters = ["username" : user!,"location" : location]
-                server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "loc", Request_parameter: parameters, Request_parameter_Images: nil, status: { (request) in
+        print("ABCD")
+        let lat = String(describing: locationManager.location?.coordinate.latitude)
+        let long = String(describing: locationManager.location?.coordinate.longitude)
+        let location = lat.appending(",").appending(long)
+        let user = defaults.string(forKey: "user")
+        let parameters = ["username" : user!,"location" : location]
+        server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "loc", Request_parameter: parameters, Request_parameter_Images: nil, status: { (request) in
             
         }, response_Dictionary: { (request) in
             
