cf1aacfa5fd8bf695cda8509b88f755b67629b0f
gaddamsrikanth <gaddam.srikanth.gollapally@gmail.com>
2017-02-27 16:20:31 +0530

    Forgot password

--- FriendFinder/FriendFinder/SignUPViewController.swift
+++ FriendFinder/FriendFinder/SignUPViewController.swift
@@ -12,6 +12,8 @@ import GooglePlaces
 
 class SignUPViewController: UIViewController,GMSAutocompleteViewControllerDelegate {
 
+    
+    @IBOutlet var email: UITextField!
     @IBOutlet var lbl1: UILabel!
         @IBOutlet var city: UITextField!
         @IBOutlet var cty: UILabel!
@@ -99,7 +101,7 @@ class SignUPViewController: UIViewController,GMSAutocompleteViewControllerDelega
 
         @IBAction func signUPAction(_ sender: UIButton) {
             
-            if self.firstname.text!.isEmpty || self.lastname.text!.isEmpty || self.username.text!.isEmpty || self.pwd.text!.isEmpty || self.pwd1.text!.isEmpty || self.city.text!.isEmpty {
+            if self.firstname.text!.isEmpty || self.lastname.text!.isEmpty || self.username.text!.isEmpty || self.pwd.text!.isEmpty || self.pwd1.text!.isEmpty || self.city.text!.isEmpty || (self.email.text?.isEmpty)! {
                 let alt = UIAlertController(title: "Enter Value", message: "All Fields are Mandatory", preferredStyle: UIAlertControllerStyle.alert)
                 alt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
                 self.present(alt, animated: true, completion: nil)
@@ -110,7 +112,7 @@ class SignUPViewController: UIViewController,GMSAutocompleteViewControllerDelega
                 self.present(alt, animated: true, completion: nil)
             }else {
                 let nm = self.firstname.text?.appending(" ").appending(self.lastname.text!)
-                let parameters = ["name": nm!,"username": self.username.text!,"password": self.pwd.text!,"locality": self.city.text!]
+                let parameters = ["name": nm!,"username": self.username.text!,"password": self.pwd.text!,"locality": self.city.text!,"email" : self.email.text!]
                 print(self.city.text!)
                 server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "reg", Request_parameter: parameters, Request_parameter_Images: nil, status: { (result) in
                 
