36a9ba901176d71588eef7b8dd389271d2d42847
Deepsikha <lanetteam.deepsikha@gmail.com>
2017-02-28 18:36:35 +0530

    complete friend map with first distance threshold

--- FriendFinder/FriendFinder/tabbar.swift
+++ FriendFinder/FriendFinder/tabbar.swift
@@ -25,21 +25,25 @@ class tabbar: UITabBarController,UITabBarControllerDelegate {
     override func viewWillLayoutSubviews() {
         var tabFrame = self.tabBar.frame
         
-        tabFrame.size.height = 60
-        tabFrame.origin.y = self.view.frame.size.height - 60
+        tabFrame.size.height = 49
+        tabFrame.origin.y = self.view.frame.size.height - 49
         self.tabBar.frame = tabFrame
     }
     
     override func viewWillAppear(_ animated: Bool) {
         
         tab1 = HomeViewController()
-        tab1.tabBarItem = UITabBarItem(title: "HomeView", image: UIImage(named: "profile"), selectedImage: UIImage(named: "profile"))
-        
+        tab1.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "profile"), selectedImage: UIImage(named: "profile"))
+        tab1.tabBarItem.imageInsets  = UIEdgeInsetsMake(6, 0, -6, 0)
+
         tab2 = FriendViewController()
-        tab2.tabBarItem = UITabBarItem(title: "FriendView", image: UIImage(named: "friend"), selectedImage: UIImage(named: "friend"))
+        tab2.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "friend"), selectedImage: UIImage(named: "friend"))
+        tab2.tabBarItem.imageInsets  = UIEdgeInsetsMake(6, 0, -6, 0)
         
         tab3 = mapViewController()
-        tab3.tabBarItem = UITabBarItem(title: "mapView", image: UIImage(named: "nearby"), selectedImage: UIImage(named: "nearby"))
+        tab3.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "nearby"), selectedImage: UIImage(named: "nearby"))
+        tab3.tabBarItem.imageInsets  = UIEdgeInsetsMake(6, 0, -6, 0)
+
         
         self.viewControllers = [tab1, tab2, tab3]
         
